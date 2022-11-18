ddb8faecad495d99565b79a4225b9d969def0dbf
Magn3tism <chapagainsakshyam@gmail.com>
2022-11-18 09:07:50 +0545

    Add validation on button click

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const inputs = Array.from(document.getElementsByTagName("input"));
 const refocus = [0, 0, 0, 0, 0, 0];
 
+const create = document.getElementById("create");
+const errorTexts = Array.from(document.getElementsByClassName("error-text"));
+
 inputs.forEach((inp) => {
   let i = inputs.indexOf(inp);
   inp.addEventListener("click", () => (refocus[i] = 1));
@@ -17,5 +20,32 @@ document.body.addEventListener("click", () => {
     } else {
       inputs[i].classList.remove("error");
     }
+  }
+});
+
+inputs.forEach((inp) =>
+  inp.addEventListener(
+    "input",
+    () => (errorTexts[inputs.indexOf(inp)].textContent = "")
+  )
+);
+
+create.addEventListener("click", () => {
+  inputs.forEach((inp) => {
+    if (inp.value.trim() === "") {
+      errorTexts[inputs.indexOf(inp)].textContent = "Field cannot be empty.";
+    }
+  });
+
+  if (inputs[2].validity.typeMismatch) {
+    errorTexts[2].textContent = "Email must be in the pattern name@example.com";
+  }
+
+  if (inputs[3].validity.badInput) {
+    errorTexts[3].textContent = "Phone Number must be a numbr";
+  }
+
+  if (inputs[4].value !== inputs[5].value) {
+    errorTexts[4].textContent = "Password and Cofirmation don't match";
   }
 });
